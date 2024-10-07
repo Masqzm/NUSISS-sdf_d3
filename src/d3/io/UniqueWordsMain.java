8ee72abbe65b9736108cdc377e00568a4bfaca0c
masqzm <mhazima95@gmail.com>
2024-10-07 17:19:26 +0800

    Add CountWordsMain.java (count unique words using Map); Update ReadWriteProcMain & UniqueWordsMain (count unique words using Set)

--- src/d3/io/UniqueWordsMain.java
+++ src/d3/io/UniqueWordsMain.java
@@ -1,15 +1,14 @@
 package d3.io;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
+import java.io.BufferedReader; 
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileReader; 
 import java.io.IOException;
 import java.io.Reader;
-import java.io.Writer;
+import java.util.HashSet;
+import java.util.Set; 
 
+// Count total unique words using Set
 public class UniqueWordsMain {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         String inFile = args[0];        
@@ -17,19 +16,33 @@ public class UniqueWordsMain {
         // Open input file for reading
         Reader reader = new FileReader(inFile);
         BufferedReader br = new BufferedReader(reader);
+
+        // Create set of string
+        Set<String> uniqueWords = new HashSet<>();
         
         String line;    // to store current line being read
         
         while((line = br.readLine()) != null)       // read line if it exists in input file
         {
+            if(line.isEmpty())
+                continue;
+                
             // Process line
             String regex = "\\p{Punct}"; // matches any char in: !”#$%&'()*+,-./:;<=>?@[\]^_`{|}~:
-            String processed = line.replaceAll(regex, "");
+            String processed = line.replaceAll(regex, "").toLowerCase().trim();
+            
+            for(String word : processed.split(" "))
+                uniqueWords.add(word);
 
-            System.out.println(processed);
+            //System.out.println(processed);
         }
 
         // Close files
         reader.close();
+
+        System.out.printf("Total unique words in %s: %d\n\n", inFile, uniqueWords.size());
+
+        for(String word : uniqueWords)
+            System.out.printf("%s, ", word);
     }
 }
